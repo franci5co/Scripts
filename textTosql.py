f218535d7ce34eb9bcb59296aca02e3ed663016d
franci5co <francisco.vergara@gmail.com>
2015-02-16 15:13:57 -0300

    Se agregan archivos de proyecto PyCharm. Se reformatea y ordena el codigo de acuerdo a sugerencias del IDE.

--- textTosql.py
+++ textTosql.py
@@ -4,67 +4,68 @@ import sys
 import sqlite3
 
 
-def ctaPaginas (nombreArchivo,contador):
-	
-	with open(nombreArchivo,'r') as tempFile:
-		archivoCompleto = tempFile.readlines()
-		#print archivoCompleto
-		for line in archivoCompleto :
-			if ("NOMBRE" in line):
-				contador = contador + 1
-				#contadorLineas = contadorLineas + 1
-				#print line
-				#print contador
-			#contadorLineas = contadorLineas + 1
+def cta_paginas (nombre_archivo,contador):
+    with open(nombre_archivo,'r') as tempFile:
+        archivo_completo = tempFile.readlines()
+        # print archivo_completo
+        for line in archivo_completo :
+            if "NOMBRE" in line:
+                contador += 1
+                # contadorLineas = contadorLineas + 1
+                # print line
+                # print contador
+            # contadorLineas = contadorLineas + 1
 
-	#print contadorLineasl
-	#print contador
-	return contador
+    # print contadorLineasl
+    # print contador
+    return contador
 
-def ctaLineas (nombreArchivo,contador,pagina):
-	
-	with open(nombreArchivo,'r') as tempFile:
-		rutFlag = True
-		nomFlag = False
-		contNombre = 0
-		archivoCompleto = tempFile.readlines()
-		while (rutFlag):
-			for line in archivoCompleto :
-				print 'cNombre ' , contNombre
-				print 'pagina ' , pagina
-				print 'nomFlag ' ,  nomFlag
-				print 'linea ' , line
-				print 'contador ', contador
-				print 'rutFlag ', rutFlag
-				if ("NOMBRE" in line):
-					contNombre = contNombre + 1
-				if ("C.IDENTIDAD" in line and contNombre == pagina):
-					rutFlag = False
-					break
-				if ("NOMBRE" in line and contNombre == pagina):
-					nomFlag = True
-					continue
-				if (line.strip() and nomFlag):
-					contador = contador + 1
-				#contNombre = contNombre + 1
 
-	return contador
+def cta_lineas (nombre_archivo,contador,pagina):
+    with open(nombre_archivo,'r') as tempFile:
+        rut_flag = True
+        nom_flag = False
+        cont_nombre = 0
+        archivo_completo = tempFile.readlines()
+        while rut_flag:
+            for line in archivo_completo :
+                print 'cNombre ' , cont_nombre
+                print 'pagina ' , pagina
+                print 'nom_flag ' ,  nom_flag
+                print 'linea ' , line
+                print 'contador ', contador
+                print 'rut_flag ', rut_flag
+                if "NOMBRE" in line:
+                    cont_nombre += 1
+                if "C.IDENTIDAD" in line and cont_nombre == pagina:
+                    rut_flag = False
+                    break
+                if "NOMBRE" in line and cont_nombre == pagina:
+                    nom_flag = True
+                    continue
+                if line.strip() and nom_flag:
+                    contador += 1
+                    # cont_nombre = cont_nombre + 1
 
-def insertaSQL (nombreUsuario,rutUsuario,dvUsuario,dirUsuario,archivoActual):
-	conn = sqlite3.connect('usuarios')
-	c = conn.cursor()
-	valores = [nombreUsuario,rutUsuario,dvUsuario,dirUsuario,archivoActual]
-	c.execute('INSERT INTO alfa VALUES (?,?,?,?,?)', valores,)
-	conn.commit()
-	conn.close()
+    return contador
 
-def formateaRUT (rut):
-	rut = rut.split('-')
-	ci = rut[0].replace(".","")
-	dv = rut[1]
-	return (ci,dv)
 
-#def llenaMatrizTemporal () :
+def inserta_sql(nombre_user, rut_user, dv_user, dir_user, file_actual):
+    conn = sqlite3.connect('usuarios')
+    c = conn.cursor()
+    valores = [nombre_user, rut_user, dv_user, dir_user, file_actual]
+    c.execute('INSERT INTO alfa VALUES (?,?,?,?,?)', valores,)
+    conn.commit()
+    conn.close()
+
+
+def parse_rut (rut):
+    rut = rut.split('-')
+    ci = rut[0].replace(".", "")
+    dv = rut[1]
+    return ci, dv
+
+# def llenaMatrizTemporal () :
 
 cont = 0
 cont2 = 0
@@ -74,52 +75,49 @@ mi_dv = ''
 nuevaCuenta = 0
 bandera = False
 
-
-
 archivosLista = os.listdir("/home/francisco/PDF")
 
 tempMatrix = [[] for _ in range(70)]
 
-#tempMatrix =  [ [ range(5) ] for i in range(70) ] 
-#print tempMatrix 
-#tempMatrix[0].append("jose francisco")
-#print tempMatrix
-#tempMatrix[4].append(13212057)
-#print tempMatrix
-#for x in range(0,69)
-	
-#contador = ctaPaginas("/home/francisco/PDF/A1249004.pdf.txt",cont)
-contadorLineas = ctaLineas("/home/francisco/PDF/A1249004.pdf.txt", cont2, pagina)
+# tempMatrix =  [ [ range(5) ] for i in range(70) ]
+# print tempMatrix
+# tempMatrix[0].append("jose francisco")
+# print tempMatrix
+# tempMatrix[4].append(13212057)
+# print tempMatrix
+# for x in range(0,69)
+# contador = cta_paginas("/home/francisco/PDF/A1249004.pdf.txt",cont)
 
-#print contador
+contadorLineas = cta_lineas("/home/francisco/PDF/A1249004.pdf.txt", cont2, pagina)
+
+# print contador
 print 'pagina ',pagina,' lineas ', contadorLineas
 
-mi_rut, mi_dv = formateaRUT('13.212.057-k')
+mi_rut, mi_dv = parse_rut('13.212.057-k')
 print mi_rut,mi_dv
 
 
 with open("/home/francisco/PDF/A1249004.pdf.txt",'r') as tempFile:
-	archivoCompleto = tempFile.readlines()
-	for linea in archivoCompleto:
-		if ("NOMBRE" in linea and nuevaCuenta == 0):
-			bandera = True
-			continue
+    archivo_completo = tempFile.readlines()
+    for linea in archivo_completo:
+        if "NOMBRE" in linea and nuevaCuenta == 0:
+            bandera = True
+            continue
 
-		if (bandera == False):
-			continue
+        if not bandera:
+            continue
 
-		if (bandera):
-			tempMatrix[nuevaCuenta].append(linea)
-			nuevaCuenta = nuevaCuenta + 1
-			if (nuevaCuenta > 69):
-				break
-
+        if bandera:
+            tempMatrix[nuevaCuenta].append(linea)
+            nuevaCuenta += 1
+            if nuevaCuenta > 69:
+                break
 
 print tempMatrix
 
-##insertaSQL('jose francisco vergara',13212057,'9','eleuterio ramirez 875', 'A1249004')
+# inserta_sql('jose francisco vergara',13212057,'9','eleuterio ramirez 875', 'A1249004')
 
-#with open("/home/francisco/PDF/A1249004.pdf.txt",'r') as f:
-#	data = f.readlines()
-#	for linea in data:
-		#print linea.rstrip('\n')  #quita el salto de linea al final de la linea
+# with open("/home/francisco/PDF/A1249004.pdf.txt",'r') as f:
+#   data = f.readlines()
+#   for linea in data:
+#   print linea.rstrip('\n')  #quita el salto de linea al final de la linea
