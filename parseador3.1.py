601fab91de0d1ac86ab8b2b784cebf74ed83bc9b
franci5co <franci5co@users.noreply.github.com>
2016-02-24 18:59:02 -0300

    Version Final
    ordena en un CSV, la URL, el tipo de CMS y la version obtenidos de un archivo de reporte de auditoria.

---
+++ parseador3.1.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+import os
+contador = 0
+sc = 0
+archivos = filter(os.path.isfile, os.listdir(os.curdir))
+archivoout = open('salida.csv', 'w')
+for f in archivos:
+    ruta_archivo = os.curdir
+    path_archivo = os.getcwd()
+    if f != "parseador3.1.py" and f != "parseador3.py":
+        with open(path_archivo + "/" + f, 'r') as origen:
+            for linea in origen:
+                contador += 1
+                #  print linea
+                if "Scanning" in linea:
+                    sc = 1
+                    url = linea.split()
+                if "CMS" in linea and sc == 1:
+                    sc = 0
+                    cms = linea.split()
+                    if cms[2] == "|":
+                        ver = cms[1] + cms[2] + cms[3]
+                    else:
+                        ver = cms[1]
+                    url1 = url[1].replace("...", "").replace("\x1b[m", "").replace("\x1b", "").replace("[0m", "")
+                    cms1 = cms[0].replace("\x1b[m", "").replace("\x1b", "").replace("[0m", "")
+                    archivoout.write(url1 + ";" + cms1 + ";" + ver + "\n")
+
+archivoout.close()
+
+
