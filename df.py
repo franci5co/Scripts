c025f2417cbdf11ea343470776ac65d712eaf3e8
franci5co <franci5co@users.noreply.github.com>
2017-04-26 17:38:56 -0300

    Add files via upload
    Envía a través de una cuenta de Gmail el resultado del comando df

---
+++ df.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+from __future__ import with_statement
+import contextlib
+import os
+import sys
+import smtplib
+
+def sendemail(from_addr, to_addr_list, cc_addr_list,
+              subject, message,
+              login, password,
+              smtpserver='smtp.gmail.com:587'):
+    header  = 'From: %s\n' % from_addr
+    header += 'To: %s\n' % ','.join(to_addr_list)
+    header += 'Cc: %s\n' % ','.join(cc_addr_list)
+    header += 'Subject: %s\n\n' % subject
+    message = header + message
+
+    server = smtplib.SMTP(smtpserver)
+    server.starttls()
+    server.login(login,password)
+    problems = server.sendmail(from_addr, to_addr_list, message)
+    server.quit()
+    return problems
+
+mensaje = "PO UdeConce Desarrollo\n\n"
+mensaje += "Filesystem   Montado en         Uso%  \n"
+with contextlib.closing(open('/etc/mtab')) as fp:
+        for m in fp:
+                fs_spec, fs_file, fs_vfstype, fs_mntops, fs_freq, fs_passno = m.split()
+                if fs_spec.startswith('/'):
+                        r = os.statvfs(fs_file)
+                        block_usage_pct = 100.0 - (float(r.f_bavail) / float(r.f_blocks) * 100)
+                        inode_usage_pct = 100.0 - (float(r.f_favail) / float(r.f_files) * 100)
+                        mensaje += "%-12s %-16s %5d\n" % (fs_spec, fs_file, block_usage_pct)
+
+sendemail(from_addr    = 'monitoraservices@gmail.com',
+          to_addr_list = ['francisco.vergara@aservicorp.com'],
+          cc_addr_list = [''],
+          subject      = 'ESPACIO UdeConce PO DES',
+          message      = mensaje,
+          login        = 'monitoraservices',
+          password     = 'Ppql2016')
